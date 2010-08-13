d89360f23d7b899d25d6a63337f82e7d6a094d63
Shenouda Bertel <ShenoudaB@gmail.com>
2010-08-13 13:00:10 +0200

    Modify CouchRest Model Template

--- lib/generators/couchrest_model/model/templates/model.rb
+++ lib/generators/couchrest_model/model/templates/model.rb
@@ -1,10 +1,14 @@
-class <%= class_name %><%= options[:parent] != nil ? "< #{options[:parent].classify}" : "< CouchRest::Model::Base" %>
+class <%= class_name %><%= options[:parent] != nil ? " < #{options[:parent].classify}" : " < CouchRest::Model::Base" %>
 
   #use_database '<%= class_name.underscore %>'
+  #unique_id :'<%= model_attributes.first.name -%>'
 
 <% model_attributes.each_with_index do |attribute, i| -%>
   property :<%= attribute.name %><%= ", :type => #{attribute.type_class}" %>
 <% end -%>
+
 <%= statements %>
+
+  #view_by :'<%= model_attributes.first.name -%>'
 
 end
